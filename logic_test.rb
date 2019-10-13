de61bfafe3eec1974b704569ce734ac56d03f6d5
Taka <hikijiri@throo.io>
2019-10-14 00:15:10 +0900

    ロジックのテスト

--- logic_test.rb
+++ logic_test.rb
@@ -3,15 +3,19 @@ material_array = ['硫酸', '二酸化炭素', '酸素']
 
 number_of_questions = 4
 
-avogadro_constant = 6.0 * 10 **23
+avogadro_constant = 6.0 * (10 **23)
 
 # 質量からmolを求める問題
-rand_material_array = rand(0..material_hash.length - 1)
-rand_mol = rand(10..100)
-material = material_array[rand_material_array]
-mass_to_mol_sentence = "#{material}が#{material_hash[material]}gあります。何molですか?"  # hashが使えていない
+material = ""
+rand_mol = 0
+
+mass_to_mol_sentence = "#{material}が#{material_hash[material.to_sym].to_f * rand_mol}gあります。何molですか?"
+p rand_mol
 
 def mass_to_mol(material_array, material_hash, mass_to_mol_sentence)
+    rand_material_array = rand(0..material_hash.length - 1)
+    rand_mol = rand(10..100) / 10.00
+    material = material_array[rand_material_array]
     return mass_to_mol_sentence
 end
 
@@ -31,5 +35,3 @@ end
     p mass_to_mol(material_array, material_hash, mass_to_mol_sentence)
     # p mol_to_mass(material_array, material_hash)
 end
-
-
