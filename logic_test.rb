45f11c566bcb20b05f990bfadd503781786b6fc3
Taka <hikijiri@throo.io>
2019-10-13 23:46:51 +0900

    ロジックのテスト

--- logic_test.rb
+++ logic_test.rb
@@ -5,8 +5,10 @@ number_of_questions = 4
 
 avogadro_constant = 6.0 * 10 **23
 
-def mass_to_mol(material_array, material_hash)
-    return "#{material_array[rand(0..material_hash.length - 1)]}が#{material_hash[:硫酸].to_f / rand(10..100)}gあります。何molですか?"
+mass_to_mol_sentence = "#{material_array[rand(0..material_hash.length - 1)]}が#{material_hash[:硫酸].to_f / rand(10..100)}gあります。何molですか?"
+
+def mass_to_mol(material_array, material_hash, mass_to_mol_sentence)
+    return mass_to_mol_sentence
 end
 
 def mol_to_mass(material_array, material_hash)
@@ -14,7 +16,7 @@ def mol_to_mass(material_array, material_hash)
 end
 
 def number_to_mol
-
+    return "#{material_array[rand(0..material_hash.length - 1)]}が#{rand(10..100).to_f / 10}molあります。何gですか?"
 end
 
 def mol_to_number
@@ -22,8 +24,8 @@ def mol_to_number
 end
 
 (1..number_of_questions).each do |number|
-    # p mass_to_mol(material_array, material_hash)
-    p mol_to_mass(material_array, material_hash)
+    p mass_to_mol(material_array, material_hash, mass_to_mol_sentence)
+    # p mol_to_mass(material_array, material_hash)
 end
 
 
