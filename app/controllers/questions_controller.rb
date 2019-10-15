c85f2767dff46a02df0c5e2ea9d740c09e9b1276
Taka <hikijiri@throo.io>
2019-10-16 01:56:04 +0900

    問題作成ページの編集

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -20,46 +20,16 @@ class QuestionsController < ApplicationController
         @question = Question.new(contents: mass_to_mol)
     end
 
+    def new_question
+        return "塩酸がたくさん...うれしい..."
+    end
+
     def create
         @question = Question.new(question_params)
         redirect_to root_path if @question.save
         render 'new' unless @question.save
     end
 
-    private
-    def question_params
-        params.require(:question).permit(:text)
-    end
-
-# 質量からmolを求める問題
-    def mass_to_mol()
-
-    Material.find(1)
-    return "hello"
-    # rand_material_array = rand(0..material_hash.length - 1)
-    # rand_mol = rand(1..100) / 10.00
-    # p rand_mol
-    # material = material_array[rand_material_array]
-    # mass_to_mol_sentence = "#{material}が#{material_hash[material.to_sym].to_f * rand_mol}gあります。何molですか?"
-    # return mass_to_mol_sentence
-    end
-
-# molから質量を求める問題
-def mol_to_mass(material_array, material_hash)
-    rand_material_array = rand(0..material_hash.length - 1)
-    rand_mol = rand(1..100) / 10.00
-    material = material_array[rand_material_array]
-    mol_to_mass_sentence = "#{material}が#{rand_mol}molあります。何molですか?"
-    return mol_to_mass_sentence, material_hash[material.to_sym].to_f * rand_mol
-end
-
-def number_to_mol
-    return "#{material_array[rand(0..material_hash.length - 1)]}が#{rand(10..100).to_f / 10}molあります。何gですか?"
-end
-
-def mol_to_number
-
-end
-
+    helper_method :new_question
     
 end
